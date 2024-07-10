e31abf8afdd8efb9cbd6d6cc9e37af95ff3d7bb0
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-07-11 00:05:43 +0530

    @PathVariable and @RequestParam code added...

--- src/main/java/com/thecoderstv/springboot/controller/UserController.java
+++ src/main/java/com/thecoderstv/springboot/controller/UserController.java
@@ -1,11 +1,8 @@
 package com.thecoderstv.springboot.controller;
 
-import com.thecoderstv.springboot.bean.User;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -15,5 +12,22 @@ public class UserController {
     @ResponseBody
     public String welcomeUser() {
         return "welcome back user";
+    }
+
+    // localhost:8080/user/about-user/25
+    // @PathVariable Annotation
+    @RequestMapping("/about-user/{age}/{name}/{city}/{pincode}")
+    @ResponseBody
+    public String aboutUser(@PathVariable String age, @PathVariable String name,
+                            @PathVariable String city, @PathVariable int pincode) {
+        return "Hello " + name + " your age is = " + age + " city = " + city + " pincode = " + pincode;
+    }
+
+    // @RequestParam
+    // localhost:8080/user/about-user?username=shubham&password=shubh123
+    @RequestMapping("/about-user")
+    @ResponseBody
+    public String aboutUserQueryParam(@RequestParam String username , @RequestParam String password){
+        return "Your username = "+username+" password = "+password;
     }
 }
