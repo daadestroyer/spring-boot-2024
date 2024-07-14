2244f8b8ef21f670df176f60ccd4efcdb1ba5008
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-07-14 17:05:37 +0530

    delete data and update data into db using spring data jpa

--- src/main/java/com/thecoderstv/springboot/SpringbootApplication.java
+++ src/main/java/com/thecoderstv/springboot/SpringbootApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Optional;
+
 // @SpringBootApplication = @Configuration + @EnableAutoConfiguration + @ComponentScan
 @SpringBootApplication
 public class SpringbootApplication {
@@ -19,19 +21,57 @@ public class SpringbootApplication {
 //       ApplicationNamePrinter app = context.getBean("applicationNamePrinter", ApplicationNamePrinter.class);
 //        System.out.println(app);
 
+
+        UserRepositories userRepositories = context.getBean(UserRepositories.class);
+
+
         // saving user to database
         /*
-        UserRepositories userRepositories = context.getBean(UserRepositories.class);
         User user = new User();
-        user.setUserid(102);
-        user.setFirstname("pankaj");
+        user.setUserid(104);
+        user.setFirstname("rajni");
         user.setLastname("nigam");
-        user.setEmail("pankaj@gmail.com");
-        user.setPincode("208001");
+        user.setEmail("rajni@gmail.com");
+        user.setPincode("208005");
 
         User save = userRepositories.save(user);
         System.out.println(user);
         */
+
+        // deleting data from database
+        // using id
+        /*
+        userRepositories.deleteById(104);
+        System.out.println("data deleted");
+        */
+
+        // using object
+        /*
+        User user = new User();
+        user.setUserid(103);
+        user.setFirstname("raj");
+        user.setLastname("nigam");
+        user.setEmail("raj@gmail.com");
+        user.setPincode("208000");
+        userRepositories.delete(user);
+        System.out.println("user deleted...");
+        */
+
+        // update records
+        Optional<User> optionalUser = userRepositories.findById(102);
+        if(optionalUser.isPresent()){
+            User dbUser = optionalUser.get();
+            dbUser.setFirstname("pankaj new");
+            dbUser.setLastname("nigam new");
+            dbUser.setPincode("208000");
+            dbUser.setEmail("pankajnew@gmail.com");
+            User updatedUser = userRepositories.save(dbUser);
+            System.out.println(updatedUser);
+        }
+        else{
+            System.out.println("no user found");
+        }
+
 
     }
 }
