ad30110c8f891667c56babd4016c71f9f6d9e795
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-07-14 16:54:37 +0530

    save data into db using spring data jpa

--- src/main/java/com/thecoderstv/springboot/SpringbootApplication.java
+++ src/main/java/com/thecoderstv/springboot/SpringbootApplication.java
@@ -1,6 +1,10 @@
 package com.thecoderstv.springboot;
 
 import com.thecoderstv.springboot.applicationproperties.ApplicationNamePrinter;
+import com.thecoderstv.springboot.bean.User;
+import com.thecoderstv.springboot.repositories.UserRepositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,13 +13,25 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class SpringbootApplication {
 
-	public static void main(String[] args) {
 
-		ConfigurableApplicationContext context = SpringApplication.run(SpringbootApplication.class, args);
-//		ApplicationNamePrinter app = context.getBean("applicationNamePrinter", ApplicationNamePrinter.class);
-//		System.out.println(app);
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringbootApplication.class, args);
+//       ApplicationNamePrinter app = context.getBean("applicationNamePrinter", ApplicationNamePrinter.class);
+//        System.out.println(app);
 
+        // saving user to database
+        /*
+        UserRepositories userRepositories = context.getBean(UserRepositories.class);
+        User user = new User();
+        user.setUserid(102);
+        user.setFirstname("pankaj");
+        user.setLastname("nigam");
+        user.setEmail("pankaj@gmail.com");
+        user.setPincode("208001");
 
-	}
+        User save = userRepositories.save(user);
+        System.out.println(user);
+        */
 
+    }
 }
